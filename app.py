b3337adb1a5faa367ba347c421485337fb32eeff
Peter Stockings <pstockings@jadeworld.com>
2022-11-27 14:21:41 +1100

    Remove is_filtered flag from /person template

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ def get_person(person_id):
     person['FilteredExercises'] = list(filtered_exercises)
     if htmx:
         return render_template('partials/page/person.html',
-                               person=person, is_filtered=True, selected_exercise_ids=active_exercise_ids, max_date=max_date, min_date=min_date), 200, {"HX-Trigger": "updatedPeople"}
+                               person=person, selected_exercise_ids=active_exercise_ids, max_date=max_date, min_date=min_date), 200, {"HX-Trigger": "updatedPeople"}
 
-    return render_template('person.html', person=person, is_filtered=True, selected_exercise_ids=active_exercise_ids, max_date=max_date, min_date=min_date)
+    return render_template('person.html', person=person, selected_exercise_ids=active_exercise_ids, max_date=max_date, min_date=min_date)
 
 
 @ app.route("/person/<int:person_id>/workout", methods=['POST'])
@@ -88,7 +88,7 @@ def delete_workout(person_id, workout_id):
     db.delete_workout(workout_id)
     person = db.get_person(person_id)
     return render_template('partials/page/person.html',
-                           person=person, is_filtered=False), 200, {"HX-Trigger": "updatedPeople", "HX-Push": url_for('get_person', person_id=person_id)}
+                           person=person), 200, {"HX-Trigger": "updatedPeople", "HX-Push": url_for('get_person', person_id=person_id)}
 
 
 @ app.route("/person/<int:person_id>/workout/<int:workout_id>/start_date_edit_form", methods=['GET'])
